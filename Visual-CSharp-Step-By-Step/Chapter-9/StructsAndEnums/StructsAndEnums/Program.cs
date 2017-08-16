84837f5f2047f58a1a2beb6daf29358bec666056
Tiago Pariz <tiagopariz@gmail.com>
2017-08-16 05:35:39 -0300

    Estrutura

--- Visual-CSharp-Step-By-Step/Chapter-9/StructsAndEnums/StructsAndEnums/Program.cs
+++ Visual-CSharp-Step-By-Step/Chapter-9/StructsAndEnums/StructsAndEnums/Program.cs
@@ -12,14 +12,28 @@ namespace StructsAndEnums
     {
         static void DoWork()
         {
+            // Escreve January
             Month first = Month.January;
             Console.WriteLine(first);
+
+            // Incrementa e escreve February
             first++;
             Console.WriteLine(first);
+
+            // Escreve o último item da enum
             first = Month.December;
             Console.WriteLine(first);
             first++; // Incrementa ao terminar a enumeração
             Console.WriteLine(first); // Escreve 12
+            Console.WriteLine();
+
+            // Escreve uma data default
+            Date defaultDate = new Date();
+            Console.WriteLine(defaultDate);
+
+            // Escreve uma data
+            Date weddingAnniversary = new Date(2010, Month.July, 4);
+            Console.WriteLine(weddingAnniversary);
         }
 
         static void Main()
